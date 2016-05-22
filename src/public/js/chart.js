ce208a4e6213931ab0ce8c53d719a3378f854fc4
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-05-22 15:30:51 -0500

    Refactor on chart.js

--- src/public/js/chart.js
+++ src/public/js/chart.js
@@ -4,7 +4,6 @@
 
 $(document).ready(() =>{
   $('#form_file_name').submit(create_chart);
-
   // google.charts.load("current", {packages:["controls"]});
   google.charts.load('current', {'packages':['corechart', 'controls']});
   notify_user("Welcome to <b>HpLabs Chart Project</b> to get started write the name of the textfile", 'info');
@@ -19,9 +18,6 @@ $(document).ready(() =>{
           timer: 4000
       });
   }
-
-
-
 
 //------------------------------------------------------------------------------
   function create_chart(event){
@@ -43,74 +39,38 @@ $(document).ready(() =>{
       }
     });
   }
-
-
-
 });
 
 //------------------------------------------------------------------------------
 
-
-
-
-// function generate_chart(file_title, chart_data){
-//   $('#chart_title').text(file_title);
-//   google.charts.setOnLoadCallback(function(){
-//     var data = google.visualization.arrayToDataTable(chart_data);
-//
-//
-//     var options = {
-//       title: "Histogram",
-//       legend: { position: 'none' },
-//       animation:{
-//         startup: true,
-//         easing: 'in',
-//       },
-//
-//     };
-//     var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
-//     chart.draw(data, options);
-//   });
-//
-// }
 function generate_chart(file_title, chart_data){
   $('#chart_title').text(file_title);
   // google.charts.load("current", {packages:["controls"]});
   google.charts.setOnLoadCallback(function(){
     var data = google.visualization.arrayToDataTable(chart_data);
-
-
-
     var dashboard = new google.visualization.Dashboard(document.getElementById('dashboard_div'));
+    var rangeSlider = new google.visualization.ControlWrapper({
+      'controlType': 'NumberRangeFilter',
+      'containerId': 'filter_div',
+      'options': {
+        'filterColumnLabel': 'Value',
+        },
+      'state': {'lowValue': -2.0, 'highValue': 2.0}
+      });
 
+    // Create the histogram with the data provided
+    var histogram = new google.visualization.ChartWrapper({
+      'chartType': 'Histogram',
+      'containerId': 'chart_div',
+      'options': {
+        'legend': 'right'
+      },
+      'view': {'columns': [0,1]}
+    });
 
-        var donutRangeSlider = new google.visualization.ControlWrapper({
-          'controlType': 'NumberRangeFilter',
-          'containerId': 'filter_div',
-          'options': {
-            'filterColumnLabel': 'Value',
-          },
-          'state': {'lowValue': 0.0, 'highValue': 0.5}
-        });
-
-        // Create a pie chart, passing some options
-        var histogram = new google.visualization.ChartWrapper({
-          'chartType': 'Histogram',
-          'containerId': 'chart_div',
-          'options': {
-            'legend': 'right'
-          },
-          'view': {'columns': [0,1]}
-        });
-
-        // Establish dependencies, declaring that 'filter' drives 'histogram',
-        // so that the pie chart will only display entries that are let through
-        // given the chosen slider range.
-        dashboard.bind(donutRangeSlider, histogram);
-
-        // Draw the dashboard.
-        dashboard.draw(data);
-
+    dashboard.bind(rangeSlider, histogram);
+    // Draw the dashboard.
+    dashboard.draw(data);
   });
 
 }
