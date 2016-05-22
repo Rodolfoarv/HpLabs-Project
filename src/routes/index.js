283d14b5a5d528c5bedb4cf86ea9b9bf41efa48f
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-05-21 23:27:12 -0500

    Modified index with the specified html, css structure

---
+++ src/routes/index.js
@@ -0,0 +1,31 @@
+'use strict';
+var express = require('express');
+var generator = require("../cpp/build/Release/generator");
+var chart = require("../public/js/chart_util")
+
+var router = express.Router();
+
+function promisify(fun) {
+  return function (/* ... */) {
+    return new Promise((resolve, reject) => {
+      let args = Array.prototype.slice.call(arguments);
+      args.push((err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+      fun.apply(null, args);
+    });
+  };
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // let result = {chart : []};
+  var generated_chart = chart.generate_chart(generator.get_chart_data("9.txt"));
+  // res.json(result);
+  res.render('index', { title: 'Express',
+                        chart: generated_chart});
+});
+
+
+module.exports = router;
